27f4055421cea70dc135fbabf87b1626ecb0fc5a
Theovady Moutty <theomoutty@gmail.com>
2021-03-17 15:07:03 +0100

    add table view not functional

--- iOS_Crypto/ViewController.swift
+++ iOS_Crypto/ViewController.swift
@@ -10,11 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    var crypto = [Any]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let url = URL(string: "https://api.coingecko.com/api/v3/coins/list")!
+        let url = URL(string: "https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false")!
         
         let config = URLSessionConfiguration.default
         let sessions = URLSession(configuration: config)
@@ -27,14 +30,46 @@ class ViewController: UIViewController {
                     if let data = json as? [[String: AnyObject]] {
                             for coin in data {
                                 print(coin["name"]!)
+                                self.crypto.append(coin)
+                                DispatchQueue.main.async {
+                                    self.tableView.reloadData()
+                                }
                         }
                     }
                 }
             }
         }
         task.resume()
+        
+        
     }
     
+    class coinTableView: UITableViewCell {
+        @IBOutlet weak var labelCoin: UILabel!
+        @IBOutlet weak var imageCoin: UIImageView!
+        
+    }
+    
+    // Number of Sections In Table
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+
+    // Number of Rows in each Section
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return crypto.count
+    }
+
+    // Sets the content of each cell
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "coinCell", for: indexPath as IndexPath)
+        cell.textLabel?.text = crypto[indexPath.row] as? String
+        return cell
+
+    }
+
     
     
     /*
