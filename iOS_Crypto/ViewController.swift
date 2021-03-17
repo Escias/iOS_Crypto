9b9618859297efbdbcc3ee92a8c5c0808c4a9c78
Theovady Moutty <theomoutty@gmail.com>
2021-03-17 11:44:06 +0100

    add request api

--- iOS_Crypto/ViewController.swift
+++ iOS_Crypto/ViewController.swift
@@ -9,12 +9,55 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let url = URL(string: "https://api.coingecko.com/api/v3/coins/list")!
+        
+        let config = URLSessionConfiguration.default
+        let sessions = URLSession(configuration: config)
+       
+        let task = sessions.dataTask(with: url) { (data, response, error) in
+            if error != nil {
+                print(error!.localizedDescription)
+            } else {
+                if let json = try? JSONSerialization.jsonObject(with: data!, options: .mutableContainers) {
+                    if let data = json as? [[String: AnyObject]] {
+                            for coin in data {
+                                print(coin["name"]!)
+                        }
+                    }
+                }
+            }
+        }
+        task.resume()
     }
-
+    
+    
+    
+    /*
+    getJson { (json) in
+        print("finised")
+    }
+    showError()
+    
+    func getJson(completion: @escaping (Response) -> ()) {
+        let UrlString = "https://api.coingecko.com/api/v3/coins/list"
+        if let url = URL(string: UrlString) {
+            URLSessionTask.shared.dataTask(with: url) {
+                data, res, err in if let data = data
+                print("hey")
+                
+                let decoder = JSONDecoder()
+                if let json = try? decoder.decode(Response.self, from: data) {
+                    completion(json)
+                }
+            }
+        }.resume()
+    }
+ */
 
 }
 
