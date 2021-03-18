bb644603485024ec8927ed23fad6342e4060801b
Theovady Moutty <theomoutty@gmail.com>
2021-03-18 17:53:05 +0100

    second page not functional

--- iOS_Crypto/DetailViewController.swift
+++ iOS_Crypto/DetailViewController.swift
@@ -8,29 +8,41 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
+class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
     @IBOutlet weak var TitleCrypto: UILabel!
-    @IBOutlet weak var cryptoDetailCell: UITableView!
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var imageCrypto: UIImageView!
-    @IBOutlet weak var nameCrypto: UILabel!
     
     var image = UIImage()
     var name = ""
     var id = ""
     
+    struct cryptoDetail {
+       
+        let symbol: String
+        //let current_price: String
+        //let market_cap_rank: String
+        //let price_change_percentage_24h: String
+        let last_updated: String
+        let total_volume: String
+        let high_24h: String
+        let low_24h: String
+    }
+
+    var cryptoD = [cryptoDetail]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        nameCrypto.text = name
+        TitleCrypto.text = name
         imageCrypto.image = image
-        print(id)
-
-        let id = "bitcoin"
+        
+        
         let url = URL(string: "https://api.coingecko.com/api/v3/coins/\(id)")!
         
         let config = URLSessionConfiguration.default
         let sessions = URLSession(configuration: config)
-       
+        
         let task = sessions.dataTask(with: url) { (data, response, error) in
             if error != nil {
                 print(error!.localizedDescription)
@@ -38,18 +50,50 @@ class DetailViewController: UIViewController {
                 
                 if let json = try? JSONSerialization.jsonObject(with: data!, options: .mutableContainers) {
                     if let data = json as? [String: AnyObject] {
-                        DispatchQueue.main.async{
-                            self.TitleCrypto.text = data["name"]! as? String
+                            self.cryptoD.append(cryptoDetail(symbol: data["symbol"] as! String,
+                                           last_updated: data["last_updated"] as! String ,
+                                           total_volume: data["total_volume"] as! String,
+                                           high_24h: data["high_24h"] as! String,
+                                           low_24h: data["low_24h"] as! String))
+                            DispatchQueue.main.async {
+                                self.tableView.reloadData()
+                            }
                         }
                     }
                 }
             }
-        }
+        
         task.resume()
         
-
+        //current_price: data["market_data"]!["current_price"] as! String,
+        //market_cap_rank: data["market_cap_rank"] as! String,
+        //price_change_percentage_24h: data["price_change_percentage_24h"] as! String,
         
     }
+    
+   func numberOfSections(in tableView: UITableView) -> Int {
+       return 1
+   }
+   // Number of Rows in each Section
+   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       return self.cryptoD.count
+   }
+
+   
+   // Sets the content of each cell
+   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+       let cell = tableView.dequeueReusableCell(withIdentifier: "detailCell", for: indexPath as IndexPath)
+        cell.textLabel?.text = self.cryptoD[indexPath.row].symbol
+        //cell.textLabel?.text = self.cryptoD[indexPath.row].current_price
+        //cell.textLabel?.text = self.cryptoD[indexPath.row].market_cap_rank
+        //cell.textLabel?.text = self.cryptoD[indexPath.row].price_change_percentage_24h
+        cell.textLabel?.text = self.cryptoD[indexPath.row].last_updated
+        cell.textLabel?.text = self.cryptoD[indexPath.row].total_volume
+        cell.textLabel?.text = self.cryptoD[indexPath.row].high_24h
+        cell.textLabel?.text = self.cryptoD[indexPath.row].low_24h
+
+       return cell
+   }
         // Do any additional setup after loading the view.
 }
     
